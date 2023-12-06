87e1bd7b791c76f96bc2b22614ed14ee7f5d63f9
camargodev <camargodev@gmail.com>
2023-12-06 20:53:04 -0300

    central entities

--- day-5/src/part_1/location_finder.py
+++ day-5/src/part_1/location_finder.py
@@ -1,29 +1,9 @@
+from src.commons.entities import *
+
 SEED = "seed"
 LOCATION = "location"
 EMPTY = ""
 LINE_BREAK = "\n"
-
-class OffsetRange:
-    def __init__(self, start, end, offset):
-        self.start = start
-        self.end = end
-        self.offset = offset
-        self.range = range(start, end+1)
-    
-    def is_number_inside(self, number):
-        return number in self.range
-    
-class MappingInfo:
-    def __init__(self, source, target):
-        self.source = source
-        self.target = target
-        self.offset_ranges = []
-    
-    def add_offset_range(self, offset_range):
-        self.offset_ranges.append(offset_range)
-
-    def sort_ranges(self):
-        self.offset_ranges.sort(key=lambda range: range.start)
 
 class LowestLocationFinder:
 
