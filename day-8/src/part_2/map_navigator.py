8f5679afa22a1725708d7c679071ef0ab8b88a91
camargodev <camargodev@gmail.com>
2023-12-09 17:26:04 -0300

    commons

--- day-8/src/part_2/map_navigator.py
+++ day-8/src/part_2/map_navigator.py
@@ -1,36 +1,8 @@
-import re
+from src.commons.input_extractor import InputExtractor
 from math import lcm
 
-LEFT = "L"
-RIGHT = "R"
-
-class DirectionManager:
-    def __init__(self, directions):
-        self.directions = directions
-        self.current = 0
-    
-    def next(self):
-        instruction = self.directions[self.current]
-        self.current = (self.current + 1) % len(self.directions)
-        return instruction
-
-class CoordinateManager:
-    def __init__(self):
-        self.coordinates = dict()
-
-    def add_coordinate(self, source, left, right):
-        target_map = {LEFT: left, RIGHT: right}
-        self.coordinates[source] = target_map
-
-class InputExtractor:
-    def extract(self, lines):
-        direction_manager = DirectionManager(lines[0].replace("\n", ""))
-        coordinate_manager = CoordinateManager()
-        for line in lines[2:]:
-            found = re.search('(.+?) = \((.+?), (.+?)\)', line)
-            source, left, right = found.group(1), found.group(2), found.group(3)
-            coordinate_manager.add_coordinate(source, left, right)
-        return direction_manager, coordinate_manager
+GOAL_SUFFIX = "Z"
+START_SUFFIX = "A"
 
 class PathInfo:
     def __init__(self, current):
@@ -41,6 +13,16 @@ class PathInfo:
 
     def already_found_loop(self):
         return len(self.loop_path) > 0
+    
+    def process_next_step(self, next_step):
+        reached_end = next_step.endswith(GOAL_SUFFIX)
+        loop_already_found = self.already_found_loop()
+        if not reached_end and not loop_already_found:
+            self.path_until_first_end.append(next_step)
+        elif reached_end and loop_already_found:
+            self.completed_loop = True
+        else:
+            self.loop_path.append(next_step)
 
 class MapNavigator:
     def __init__(self):
@@ -49,45 +31,19 @@ class MapNavigator:
     def count_steps_to_goal(self, lines):
         direction_manager, coordinate_manager = self.input_extractor.extract(lines)
         
-        paths = [PathInfo(start) for start in self.extract_starts(coordinate_manager)]
+        sources = coordinate_manager.coordinates.keys()
+        paths = [PathInfo(source) for source in sources if source.endswith(START_SUFFIX)]
         
-        all_paths_arrived_at_goal = False
-        while not all_paths_arrived_at_goal:
+        all_paths_completed_a_loop = False
+        while not all_paths_completed_a_loop:
             next_direction = direction_manager.next()
-            all_paths_arrived_at_goal = True
+            all_paths_completed_a_loop = True
             for path_info in paths:
                 if path_info.completed_loop == True:
                     continue
-                all_paths_arrived_at_goal = False
-                current = path_info.current
-                next_step = coordinate_manager.coordinates[current][next_direction]
-
-                if not self.is_end(next_step):
-                    if not path_info.already_found_loop():
-                        path_info.path_until_first_end.append(next_step)
-                    else:
-                        path_info.loop_path.append(next_step)
-                else:
-                    if not path_info.already_found_loop():
-                        path_info.loop_path.append(next_step)
-                    else:
-                        path_info.completed_loop = True
+                all_paths_completed_a_loop = False
+                next_step = coordinate_manager.coordinates[path_info.current][next_direction]
+                path_info.process_next_step(next_step)
                 path_info.current = next_step
 
         return lcm(*[len(path.loop_path) for path in paths])
-    
-    def extract_starts(self, coordinate_manager):
-        starts = []
-        for source in coordinate_manager.coordinates.keys():
-            if source[-1] == "A":
-                starts.append(source)
-        return starts
-    
-    def arrived_at_goal(self, current_steps):
-        for current in current_steps:
-            if current[-1] != "Z":
-                return False
-        return True
-    
-    def is_end(self, step):
-        return step[-1] == "Z"
