6756cf749b76be728a39ab1c61de42664b52790b
João Vitor de Camargo <camargodev@gmail.com>
2023-12-09 17:27:25 -0300

    Day 7 ✅  (#7)
    * part-1
    
    * day 2 passing
    
    * common files
    
    * only good code
    
    * simplify card order
    
    * returning rule ste
    
    * remove unused card
    
    * correct main

---
+++ day-7/main.py
@@ -0,0 +1,10 @@
+from src.part_1.simple_bet_manager import SimpleBetManager as FirstBetManager
+from src.part_2.bet_with_joker_manager import BetWithJokerManager as SecondBetManager
+from src.bet_winnings_calculator import WinningCalculator
+
+
+if __name__ == "__main__":
+    lines = [line for line in open("day-7/res/input.txt", "r")]
+    print(WinningCalculator(FirstBetManager()).calculate(lines))
+    print(WinningCalculator(SecondBetManager()).calculate(lines))
+
