acdd3386b29b6555d0e7cfa7f15755db20c51cf8
unknown <jvdecamargo@gmail.com>
2023-12-10 18:55:31 -0300

    inner function for factory

--- day-10/src/part_2/pype_loop_polygon_manager.py
+++ day-10/src/part_2/pype_loop_polygon_manager.py
@@ -21,21 +21,23 @@ class PipeLoopPolygonManager:
         min_col, max_col = self.boundaries_by_row[row_idx]
         return range(min_col, max_col)
 
-    def fill_boundaries_by_row_dict(self):
-        for pipe in self.loop_node_set:
-            row, col = pipe
-            if row not in self.boundaries_by_row:
-                self.boundaries_by_row[row] = col, col
-                continue
-            min_col, max_col = self.boundaries_by_row[row]
-            self.boundaries_by_row[row] = min(min_col, col), max(max_col, col)
-
 class PipeLoopPolygonManagerFactory:
     @staticmethod
     def create(lines, loop_nodes):
+        def create_boundaries_by_row_dict(loop_node_set):
+            boundaries_by_row = dict()
+            for pipe in loop_node_set:
+                row, col = pipe
+                if row not in boundaries_by_row:
+                    boundaries_by_row[row] = col, col
+                    continue
+                min_col, max_col = boundaries_by_row[row]
+                boundaries_by_row[row] = min(min_col, col), max(max_col, col)
+            return boundaries_by_row
+
         pipe_polygon_manager = PipeLoopPolygonManager(loop_nodes)
         pipe_polygon_manager.loop_node_set = set(loop_nodes)
         pipe_polygon_manager.loop_polygon = Polygon(loop_nodes)
-        pipe_polygon_manager.fill_boundaries_by_row_dict()
+        pipe_polygon_manager.boundaries_by_row = create_boundaries_by_row_dict(pipe_polygon_manager.loop_node_set)
         pipe_polygon_manager.board_size = len(lines)
         return pipe_polygon_manager
