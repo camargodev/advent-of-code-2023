420746d2055289ef885aeb8b046364b14fd3539e
unknown <jvdecamargo@gmail.com>
2023-12-09 21:11:54 -0300

    part-1

---
+++ day-9/src/part_1/oasis_protector.py
@@ -0,0 +1,27 @@
+class OasisProtector():
+    def predict_values_to_save_oasis(self, lines):
+        predicted_value_sum = 0
+        for line in lines:
+            values = [int(value) for value in line.replace("\n", "").split(" ")]
+            # print(values)
+            all_differences = [values]
+            while not self.is_all_zeroes(values):
+                values = self.calculate_differences(values)
+                all_differences.append(values)
+                # print(values)
+            predicted_value = 0
+            for differences in reversed(all_differences):
+                last = differences[-1]
+                predicted_value += last
+                # print(differences, last, predicted_value)
+            predicted_value_sum += predicted_value
+        return predicted_value_sum
+
+    def calculate_differences(self, values):
+        differences = []
+        for index in range(len(values)-1):
+            differences.append(values[index+1] - values[index])
+        return differences
+    
+    def is_all_zeroes(self, values):
+        return set(values) == {0}
---
+++ day-9/src/part_2/oasis_protector.py
@@ -0,0 +1,3 @@
+class OasisProtector():
+    def predict_values_to_save_oasis(self, lines):
+        pass
