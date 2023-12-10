fb49bfe03d8e7b2cfda1e95f50a5d10b85a2f0d4
unknown <jvdecamargo@gmail.com>
2023-12-09 21:28:05 -0300

    commons

--- day-9/src/part_2/oasis_protector.py
+++ day-9/src/part_2/oasis_protector.py
@@ -1,28 +1,5 @@
-class OasisProtector():
-    def predict_values_to_save_oasis(self, lines):
-        predicted_value_sum = 0
-        for line in lines:
-            values = [int(value) for value in line.replace("\n", "").split(" ")]
-            # print(values)
-            all_differences = [values]
-            while not self.is_all_zeroes(values):
-                values = self.calculate_differences(values)
-                all_differences.append(values)
-                # print(values)
-            predicted_value = 0
-            for differences in reversed(all_differences):
-                first = differences[0]
-                predicted_value = first - predicted_value
-                # print(differences, first, predicted_value)
-            predicted_value_sum += predicted_value
-            # print(predicted_value)
-        return predicted_value_sum
+from src.commons.base_oasis_protector import BaseOasisProtector
 
-    def calculate_differences(self, values):
-        differences = []
-        for index in range(len(values)-1):
-            differences.append(values[index+1] - values[index])
-        return differences
-    
-    def is_all_zeroes(self, values):
-        return set(values) == {0}
+class OasisProtector(BaseOasisProtector):
+    def calculate_predicted_value(self, current_prediction, differences):
+        return differences[0] - current_prediction
