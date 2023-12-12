397d54672ee39f1bdaaf998d0b593695d91fe0ce
camargodev <camargodev@gmail.com>
2023-12-11 23:31:50 -0300

    generic code

--- day-11/main.py
+++ day-11/main.py
@@ -3,6 +3,6 @@ from src.part_2.galaxy_distance_calculator import GalaxyDistanceCalculator as Se
 
 if __name__ == "__main__":
     lines = [line for line in open("day-11/res/input.txt", "r")]
-    print(FirstGalaxyDistanceCalculator().calculate(lines))
-    print(SecondGalaxyDistanceCalculator().calculate(lines))
+    print(FirstGalaxyDistanceCalculator().calculate(lines, 2))
+    print(FirstGalaxyDistanceCalculator().calculate(lines, 1000000))
 
--- day-11/src/part_1/galaxy_distance_calculator.py
+++ day-11/src/part_1/galaxy_distance_calculator.py
@@ -1,31 +1,65 @@
 GALAXY = "#"
 EMPTY = "."
 
+class Galaxy:
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+
+class GalaxyPair:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+    def calculate_distance(self, empty_rows, empty_cols, empty_space_multiplier):
+        return self.calculate_base_distance() + self.calculate_empty_distance(empty_rows, empty_cols, empty_space_multiplier)
+
+    def calculate_base_distance(self):
+        return abs(self.first.row - self.second.row) + abs(self.first.col - self.second.col)
+    
+    def calculate_empty_distance(self, empty_rows, empty_cols, empty_space_multiplier):
+        min_x, max_x = min(self.first.row, self.second.row), max(self.first.row, self.second.row)
+        min_y, max_y = min(self.first.col, self.second.col), max(self.first.col, self.second.col)
+        empty_rows_count = [row for row in range(min_x, max_x) if row in empty_rows]
+        empty_cols_count = [row for row in range(min_y, max_y) if row in empty_cols]
+        return (empty_space_multiplier-1)*len(empty_rows_count) + (empty_space_multiplier-1)*len(empty_cols_count)
+
+class UniverseSizeCalculator:
+    @staticmethod
+    def calculate(lines):
+        return len(lines), len(lines[0])
+
+class Universe:
+    def __init__(self):
+        self.galaxies = []
+        self.rows_with_galaxies = set()
+        self.cols_with_galaxies = set()
+
+    def add_galaxy_at_location(self, row, col):
+        self.galaxies.append(Galaxy(row, col))
+        self.rows_with_galaxies.add(row)
+        self.cols_with_galaxies.add(col)
+
+    def calculate_total_galaxy_distance(self, universe_size, empty_space_multiplier):
+        num_rows, num_cols = universe_size
+        empty_rows = set([idx for idx in range(num_rows)]) - self.rows_with_galaxies
+        empty_cols = set([idx for idx in range(num_cols)]) - self.cols_with_galaxies
+        return sum([galaxy_pair.calculate_distance(empty_rows, empty_cols, empty_space_multiplier) for galaxy_pair in self.get_galaxy_pairs()])
+
+    def get_galaxy_pairs(self):
+        pairs = []
+        for galaxy_idx, galaxy in enumerate(self.galaxies): 
+            for other_galaxy in self.galaxies[galaxy_idx+1:]:
+                pairs.append(GalaxyPair(galaxy, other_galaxy))
+        return pairs
+
 class GalaxyDistanceCalculator:
-    def calculate(self, lines):
-        galaxies = []
-        rows_with_galaxies = set()
-        cols_with_galaxies = set()
+    def calculate(self, lines, empty_space_multiplier):
+        universe = Universe()
+        universe_size = UniverseSizeCalculator.calculate(lines)
         for row_idx, row in enumerate(lines):
             for col_idx, cell in enumerate(row):
                 if cell == GALAXY:
-                    galaxies.append((row_idx, col_idx))
-                    rows_with_galaxies.add(row_idx)
-                    cols_with_galaxies.add(col_idx)
-        
-        empty_rows = set([idx for idx in range(len(lines))]) - rows_with_galaxies
-        empty_cols = set([idx for idx in range(len(lines[0]))]) - cols_with_galaxies
+                    universe.add_galaxy_at_location(row_idx, col_idx)
 
-        total_distance = 0 
-        for galaxy_idx, galaxy in enumerate(galaxies):
-            for other_galaxy_idx, other_galaxy in enumerate(galaxies[galaxy_idx+1:]):
-                galaxy_x, galaxy_y = galaxy
-                other_galaxy_x, other_galaxy_y = other_galaxy
-                min_x, max_x = min(galaxy_x, other_galaxy_x), max(galaxy_x, other_galaxy_x)
-                min_y, max_y = min(galaxy_y, other_galaxy_y), max(galaxy_y, other_galaxy_y)
-                empty_rows_count = [row for row in range(min_x, max_x) if row in empty_rows]
-                empty_cols_count = [row for row in range(min_y, max_y) if row in empty_cols]
-                distance = (max_x - min_x + len(empty_rows_count)) + (max_y - min_y + len(empty_cols_count))
-                total_distance += distance
-
-        return total_distance
+        return universe.calculate_total_galaxy_distance(universe_size, empty_space_multiplier)
