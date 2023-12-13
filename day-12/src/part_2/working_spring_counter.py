408cb96a6c509c52ea2ee104be81bd2d255cdc41
camargodev <camargodev@gmail.com>
2023-12-13 17:29:01 -0300

    count working

--- day-12/src/part_2/working_spring_counter.py
+++ day-12/src/part_2/working_spring_counter.py
@@ -46,57 +46,53 @@ class WorkingSpringCounter:
 
     def process_spring(self, spring):
         all_possible_patterns = []
-        self.process_spring_dp(all_possible_patterns, spring)
-        return len(all_possible_patterns)
+        # self.process_spring_dp(all_possible_spring)
+        # return len(all_possible_patterns)
+        return self.process_spring_dp(spring)
 
-    def process_spring_dp(self, patterns, spring, result="", current_pattern_idx=0, current_num_idx=0, characters_added=0):
+    def process_spring_dp(self, spring, result="", current_pattern_idx=0, current_num_idx=0, characters_added=0):
         if current_num_idx == len(spring.numbers) and SPRING in spring.pattern[current_pattern_idx:]:
-            return
+            return 0
         
         if current_num_idx > len(spring.numbers) or current_pattern_idx > len(spring.pattern):
-            return
+            return 0
         
         if current_num_idx < len(spring.numbers):
             current_number = spring.numbers[current_num_idx]
             missing_chars_for_number = current_number - characters_added
             chars_remaining_in_pattern = len(spring.pattern) - current_pattern_idx
             if chars_remaining_in_pattern < missing_chars_for_number:
-                return
+                return 0
         
         if current_pattern_idx == (len(spring.pattern)):
             if result[-1] == SPRING:
                 if current_num_idx == (len(spring.numbers)-1) and characters_added == spring.numbers[current_num_idx]:
-                    patterns.append(result)
-                    return
+                    return 1
             if result[-1] == EMPTY:
                 if current_num_idx == (len(spring.numbers)) and characters_added == 0:
-                    patterns.append(result)
-                    return
-            return
+                    return 1
+            return 0
+        
         if spring.pattern[current_pattern_idx] == MISTERY:
             if current_num_idx == len(spring.numbers):
-                self.process_spring_dp(patterns, spring, result+EMPTY, current_pattern_idx+1, current_num_idx, 0)
-                return
+                return self.process_spring_dp(spring, result+EMPTY, current_pattern_idx+1, current_num_idx, 0)
             if spring.is_current_number_completed(current_num_idx, characters_added):
-                self.process_spring_dp(patterns, spring, result+EMPTY, current_pattern_idx+1, current_num_idx+1, 0)
-                return
+                return self.process_spring_dp(spring, result+EMPTY, current_pattern_idx+1, current_num_idx+1, 0)
+            temp = 0
             if characters_added == 0:
-                self.process_spring_dp(patterns, spring, result+EMPTY, current_pattern_idx+1, current_num_idx, characters_added)
-            self.process_spring_dp(patterns, spring, result+SPRING, current_pattern_idx+1, current_num_idx, characters_added+1)
+                temp = self.process_spring_dp(spring, result+EMPTY, current_pattern_idx+1, current_num_idx, characters_added)
+            return self.process_spring_dp(spring, result+SPRING, current_pattern_idx+1, current_num_idx, characters_added+1) + temp
         if spring.pattern[current_pattern_idx] == EMPTY:
             if current_num_idx == len(spring.numbers):
-                self.process_spring_dp(patterns, spring, result+EMPTY, current_pattern_idx+1, current_num_idx, 0)
-                return
+                return self.process_spring_dp(spring, result+EMPTY, current_pattern_idx+1, current_num_idx, 0)
             if spring.is_current_number_completed(current_num_idx, characters_added):
-                self.process_spring_dp(patterns, spring, result+EMPTY, current_pattern_idx+1, current_num_idx+1, 0)
-                return
+                return self.process_spring_dp(spring, result+EMPTY, current_pattern_idx+1, current_num_idx+1, 0)
             elif characters_added == 0:
-                self.process_spring_dp(patterns, spring, result+EMPTY, current_pattern_idx+1, current_num_idx, 0)
-                return
-            return
+                return self.process_spring_dp(spring, result+EMPTY, current_pattern_idx+1, current_num_idx, 0)
         if spring.pattern[current_pattern_idx] == SPRING:
             if current_num_idx == len(spring.numbers):
-                return
+                return 0
             if spring.is_current_number_completed(current_num_idx, characters_added):
-                return
-            self.process_spring_dp(patterns, spring, result+SPRING, current_pattern_idx+1, current_num_idx, characters_added+1)
+                return 0
+            return self.process_spring_dp(spring, result+SPRING, current_pattern_idx+1, current_num_idx, characters_added+1)
+        return 0
