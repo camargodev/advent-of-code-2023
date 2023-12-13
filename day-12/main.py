fc6edbfb418f0fe86c702a70c34707b4d46b06be
camargodev <camargodev@gmail.com>
2023-12-12 22:01:15 -0300

    part 2 never finishes

--- day-12/main.py
+++ day-12/main.py
@@ -1,5 +1,7 @@
-from src.part_1.working_spring_counter import WorkingSpringCounter
+from src.part_1.working_spring_counter import WorkingSpringCounter as FirstWorkingSpringCounter
+from src.part_2.working_spring_counter import WorkingSpringCounter as SecondWorkingSpringCounter
 
 if __name__ == "__main__":
     lines = [line for line in open("day-12/res/input.txt", "r")]
-    print(WorkingSpringCounter().count(lines))
+    print(FirstWorkingSpringCounter().count(lines))
+    print(SecondWorkingSpringCounter().count(lines))
--- day-12/src/part_2/working_spring_counter.py
+++ day-12/src/part_2/working_spring_counter.py
@@ -18,10 +18,20 @@ class InputExtractor:
         for line in lines:
             pattern, str_numbers = line.split(" ")
             numbers = [int(str_num) for str_num in str_numbers.split(",")]
-            springs.append(Spring(pattern, numbers))
+            springs.append(Spring(self.repeat_pattern(pattern), self.repeat_numbers(numbers)))
         return springs
     
-
+    def repeat_pattern(self, pattern):
+        repeated_pattern = pattern
+        for _ in range(4):
+            repeated_pattern += MISTERY + pattern
+        return repeated_pattern
+    
+    def repeat_numbers(self, numbers):
+        repeated_numbers = []
+        for _ in range(5):
+            repeated_numbers.extend(numbers)
+        return repeated_numbers
 
 class WorkingSpringCounter:
     def count(self, lines):
@@ -47,10 +57,25 @@ class WorkingSpringCounter:
     def process_spring_dp(self, patterns, numbers_by_index, spring, result, current_pattern_idx=0, current_num_idx=0, characters_added=0):
         if DEBUG:
             print(result)
+        if current_num_idx == len(spring.numbers) and SPRING in spring.pattern[current_pattern_idx:]:
+            if DEBUG:
+                print("failed because already complete numbers, but a # will exist")
+            return
+        
         if current_num_idx > len(spring.numbers) or current_pattern_idx > len(spring.pattern):
             if DEBUG:
                 print("failed because string finished")
             return
+        
+        if current_num_idx < len(spring.numbers):
+            current_number = spring.numbers[current_num_idx]
+            missing_chars_for_number = current_number - characters_added
+            chars_remaining_in_pattern = len(spring.pattern) - current_pattern_idx
+            if chars_remaining_in_pattern < missing_chars_for_number:
+                if DEBUG:
+                    print("failed because there will not be enough chars to finish pattern")
+                return
+        
         if current_pattern_idx == (len(spring.pattern)):
             if DEBUG:
                 print("finished pattern")
