b50c2aa88a602ada1c5422ea72e9ae0f75e8dd08
camargodev <camargodev@gmail.com>
2023-12-16 17:18:14 -0300

    finished day-15

--- day-15/main.py
+++ day-15/main.py
@@ -1,7 +1,7 @@
-from src.part_1.hash_calculator import HashCalculator as FirstHashCalculator
-from src.part_2.hash_calculator import HashCalculator as SecondHashCalculator
+from src.part_1.hash_calculator import HashCalculator
+from src.part_2.hashmap_calculator import HashMapCalculator
 
 if __name__ == "__main__":
-    lines = [line for line in open("day-15/res/input.txt", "r")]
-    # print(FirstHashCalculator().calculate(lines[0]))
-    print(SecondHashCalculator().calculate(lines[0]))
+    line = [line for line in open("day-15/res/input.txt", "r")][0]
+    print(HashCalculator().calculate(line))
+    print(HashMapCalculator().calculate(line))
--- day-15/src/part_1/hash_calculator.py
+++ day-15/src/part_1/hash_calculator.py
@@ -1,15 +1,6 @@
-ASCII_MAP = {chr(i):i for i in range(256)}
+from src.commons.hash_mapper import HashMapper
 
 class HashCalculator:
     def calculate(self, line):
         words = line.split(",")
-        return sum([self.calculate_word_hash_value(word) for word in words])
-    
-    def calculate_word_hash_value(self, word):
-        value = 0
-        for char in word:
-            value += ASCII_MAP[char]
-            value *= 17
-            value = value % 256
-        print(word, value)
-        return value
+        return sum([HashMapper.map(word) for word in words])
