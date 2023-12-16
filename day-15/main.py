721441c1917b3845b25b3eb83934e335dd44a013
camargodev <camargodev@gmail.com>
2023-12-16 15:24:58 -0300

    day-15 done

--- day-15/main.py
+++ day-15/main.py
@@ -1,5 +1,7 @@
-from src.part_1.hash_calculator import HashCalculator
+from src.part_1.hash_calculator import HashCalculator as FirstHashCalculator
+from src.part_2.hash_calculator import HashCalculator as SecondHashCalculator
 
 if __name__ == "__main__":
     lines = [line for line in open("day-15/res/input.txt", "r")]
-    print(HashCalculator().calculate(lines[0]))
+    # print(FirstHashCalculator().calculate(lines[0]))
+    print(SecondHashCalculator().calculate(lines[0]))
