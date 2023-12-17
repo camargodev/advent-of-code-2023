efb6b46b13b246c7c480d582c4127284b0b998a9
camargodev <camargodev@gmail.com>
2023-12-17 18:08:12 -0300

    commom code

--- day-16/src/commons/energized_path_finder.py
+++ day-16/src/commons/energized_path_finder.py
@@ -1,66 +1,16 @@
-from collections import defaultdict, deque
-import time
-
+from collections import deque
+from src.commons.tile import *
 
 LEFT="LEFT"
 RIGHT="RIGHT"
 UP="UP"
 DOWN="DOWN"
 
-EMPTY="."
-HORIZONTAL_SPLIT="-"
-VERTICAL_SPLIT="|"
-DIAGONAL="\\"
-INVERSE_DIAGIONAL="/"
-
-class Tile:
-    def __init__(self, direction, row, col):
-        self.direction = direction
-        self.row = row
-        self.col = col
-
-    def __str__(self):
-        return f"Tile(row={self.row}, col={self.col}, direction='{self.direction}')"
-
-    def __hash__(self):
-        # Using the hash of a tuple containing the attributes as a hash value
-        return hash((self.row, self.col, self.direction))
-
-    def __eq__(self, other):
-        # Checking equality based on all attributes
-        if not isinstance(other, Tile):
-            return False
-        return (
-            self.row == other.row
-            and self.col == other.col
-            and self.direction == other.direction
-        )
-
-
-class EnergizedTilesCounter:
-    def count(self, grid):
-        start_time = time.time()
-        starts = self.get_all_possible_starts(grid)
-        res = max([self.count_starting_at(grid, start) for start in starts])
-        print("--- %s seconds ---" % (time.time() - start_time))
-        return res
-    
-    def get_all_possible_starts(self, grid):
-        num_rows = len(grid)
-        num_cols = len(grid[0])
-        starts = []
-        for row in range(num_rows):
-            starts.append(Tile(RIGHT, row, 0))
-            starts.append(Tile(LEFT, row, num_cols-1))
-        for col in range(num_cols):
-            starts.append(Tile(DOWN, 0, col))
-            starts.append(Tile(UP, num_rows-1, col))
-        return starts
-    
-    def count_starting_at(self, grid, start):
+class EnergizedPathFinder:
+    def find(self, grid, source):
         visited = set()
         nodes = deque()
-        nodes.append(start)
+        nodes.append(source)
         while len(nodes) > 0:
             node = nodes.popleft()
             if node in visited:
@@ -70,7 +20,7 @@ class EnergizedTilesCounter:
             nodes.extend(next_nodes)
         
         unique_coords = set([(node.row, node.col) for node in visited])
-        return len(unique_coords)
+        return unique_coords
 
     def calculate_next_nodes(self, node, grid):
         node_value = grid[node.row][node.col]
